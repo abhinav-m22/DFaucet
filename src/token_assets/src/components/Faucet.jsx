66a6b191083153c36053f7f7cda5eebc1696f49a
abhinav-m22 <abhinavpm05@gmail.com>
2023-03-04 23:20:00 +0530

    Added Frontend

---
+++ src/token_assets/src/components/Faucet.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Faucet() {
+
+  async function handleClick(event) {
+
+  }
+
+  return (
+    <div className="blue window">
+      <h2>
+        <span role="img" aria-label="tap emoji">
+          🚰
+        </span>
+        Faucet
+      </h2>
+      <label>Get your free DAM tokens here! Claim 10,000 DAM coins to your account.</label>
+      <p className="trade-buttons">
+        <button id="btn-payout" onClick={handleClick}>
+          Gimme gimme
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default Faucet;
